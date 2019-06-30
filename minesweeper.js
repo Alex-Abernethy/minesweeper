4c9fc7187cff5acbf17634672bfeb724c2092b81
Alex-Abernethy <Alexander.Abernethy@gmail.com>
2019-06-30 20:52:16 +1200

    mine sweeper works

--- minesweeper.js
+++ minesweeper.js
@@ -4,42 +4,43 @@ document.addEventListener('DOMContentLoaded', startGame)
 var board = {
   cells: [
     {
-      row: 0, col: 0, isMine: true, hidden: true
+      row: 0, col: 0, isMine: true, isMarked: false, hidden: true
     },
     {
-      row: 0, col: 1, isMine: false, hidden: true
+      row: 0, col: 1, isMine: false, isMarked: false, hidden: true
     }, 
     {
-      row: 0, col: 2, isMine: true, hidden: true
+      row: 0, col: 2, isMine: true, isMarked: false, hidden: true
     }, 
     {
-      row: 1, col: 0, isMine: false, hidden: true
+      row: 1, col: 0, isMine: false, isMarked: false, hidden: true
     },
     {
-      row: 1, col: 1, isMine: true, hidden: true
+      row: 1, col: 1, isMine: true, isMarked: false, hidden: true
     }, 
     {
-      row: 1, col: 2, isMine: false, hidden: true
+      row: 1, col: 2, isMine: false, isMarked: false, hidden: true
     },
     {
-      row: 2, col: 0, isMine: false, hidden: true
+      row: 2, col: 0, isMine: false, isMarked: false, hidden: true
     }, 
     {
-      row: 2, col: 1, isMine: true, hidden: true
+      row: 2, col: 1, isMine: true, isMarked: false, hidden: true
     }, 
     {
-      row: 2, col: 2, isMine: false, hidden: true
+      row: 2, col: 2, isMine: false, isMarked: false, hidden: true
     }
   ]
 }
 
 function startGame () {
 
-// In startGame, above lib.initBoard(), write a for loop. This should loop through the contents of board.cells. (Remember, board.cells is an array of objects.)
-// The loop's only job should be to call countSurroundingMines once for each cell in board.cells. You'll need to pass each cell as an argument (the bit in the parentheses).
-// Assign the result of countSurroundingMines to a property on each cell object. The new property should be called surroundingMines.
+document.addEventListener("click", checkForWin);
+document.addEventListener("contextmenu", checkForWin);
+// For loop calls countSurroundingMines once for each cell in board.cells. 
+// Assigns the result of countSurroundingMines to property surroundingMines on each cell.
 
-for (i = 0; i < board.cells.length; i++) { 
+for (let i = 0; i < board.cells.length; i++) { 
   board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
 }
 
@@ -47,11 +48,31 @@ for (i = 0; i < board.cells.length; i++) {
   lib.initBoard()
 }
 
+
+function isBoardClear () {
+  for (let i = 0; i < board.cells.length; i++) {
+    var hasUnmarkedMines = board.cells[i].isMine && !board.cells[i].isMarked;
+    var hasUnrevealedNonMines = !board.cells[i].isMine && board.cells[i].hidden;
+
+    if (hasUnmarkedMines || hasUnrevealedNonMines){
+      return false;
+    }
+  }
+  return true;
+}
+
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
+
 function checkForWin () {
+
+//loop through board.cells
+
+if (isBoardClear()) {
+  lib.displayMessage('You win!')  
+}
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -61,16 +82,13 @@ function checkForWin () {
 function countSurroundingMines (cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
   var count = 0
-  for (a = 0; a < surrounding.length; a++){
-    if (surrounding[a].isMine === true) { 
+
+  for (let i = 0; i < surrounding.length; i++){
+    if (surrounding[i].isMine === true) { 
       var count = count + 1
     }
   }
   return count
 }
 
-console.log(board)
-console.log(board.cells.length)
-console.log(board.cells[1])
-console.log(board.cells[2])
-
+console.log(board.cells)
